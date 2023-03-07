681f3f1fa50f73750823581e3ae3642026d28dfa
JShelman82 <127250998+JShelman82@users.noreply.github.com>
2023-03-07 15:43:00 -0600

    Update streamlit_app.py
    Added 3 breakfast items per coarse.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Lunch Options')
-streamlit.text('Turkey Sandwich')
-streamlit.text('Roast Beef')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
