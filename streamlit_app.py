74a079b16ceca4017bffac06ee2c7b4baa19afb4
JShelman82 <127250998+JShelman82@users.noreply.github.com>
2023-03-07 15:41:25 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Lunch Options')
 streamlit.text('Turkey Sandwich')
-Streamlit.text('Roast Beef')
+streamlit.text('Roast Beef')
